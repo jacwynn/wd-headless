04ddfaf9ff0412bcd5479cae09f5ac3605fbaaff
jacwynn <jac@wynndigitalweb.com>
2019-11-27 14:09:09 -0500

    update global fonts and colors

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -12,6 +12,8 @@ const Header = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <title>Wynn Digital</title>
+
+      <link href="https://fonts.googleapis.com/css?family=Cardo:400,700|Oswald" rel="stylesheet"></link>
     </Head>
   </div>
 );

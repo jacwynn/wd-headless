83404950d8f4faed0e2ebed8b97d0a36c05f812a
jacwynn <jac@wynndigitalweb.com>
2019-11-29 16:40:50 -0500

    update styles and functionality on hero image section

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -107,8 +107,7 @@ class Index extends Component {
           <div class="overlay"></div>
           <div className="hero-image-text">
             <h1>Need Website Hosting and Support?</h1>
-            <p>Working with us will free up time for you to take on bigger iniatives</p>
-            <p>Allow us to do what we are good at, so that you can do whatever it is your good at.</p>
+            <p>Working with us will free up time for you to take on bigger iniatives. Allow us to do what we are good at, so that you can do whatever it is your good at.</p>
             <button>Request Free Consultation</button>
           </div>
         </section>

ff397fbcd4df8132a5580dc78a1518fe9c552c26
jacwynn <jac@wynndigitalweb.com>
2020-01-03 00:17:40 -0500

    add remainder files for background and intro header sections

---
+++ frontend/components/IntroHeader.js
@@ -0,0 +1,13 @@
+const IntroHeader = () => (
+    <section className="intro-header-section" id="introHeader">
+
+        <div className="intro-header">
+             <img src="/static/images/me.png" />
+            <h1>Jac Wynn Jr.</h1>
+            <h3>SFCC Front End Developer</h3>
+        </div> 
+
+    </section>
+)
+
+export default IntroHeader;

e598b45dece5695f6001596da8052e888acbea55
jacwynn <jac@wynndigitalweb.com>
2020-04-18 18:59:33 -0400

    create featured post component

---
+++ frontend/components/FeaturedPosts.js
@@ -0,0 +1,7 @@
+const FeaturedPost = () => (
+    <section className="featured-post-section" id="featured-post">
+        <h1>Featured Post Section</h1>
+    </section>
+)
+
+export default FeaturedPost;

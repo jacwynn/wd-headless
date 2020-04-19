335468c5653973dc14e58aa138dfbc4037233929
jacwynn <jac@wynndigitalweb.com>
2020-04-19 19:50:24 -0400

    fix getInitialprops; grab data for projects and posts

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import PageWrapper from '../components/PageWrapper';
 import Menu from '../components/Menu';
@@ -8,35 +7,12 @@ import Services from '../components/Services';
 import Projects from '../components/Projects';
 import FeaturedPosts from '../components/FeaturedPosts';
 
+import fetch from 'isomorphic-unfetch';
+
 const Index = props => {
 
-  const [projects, setProject] = useState([]);
-
-
-  // Index.getInitialProps = async ctx => {
-  //     //create array map of urls
-  //     let urls = [
-  //       'https://wordpress-dot-jacwynn-site-263723.appspot.com/wp-json/wp/v2/project',
-  //       'https://wordpress-dot-jacwynn-site-263723.appspot.com/wp-json/wp/v2/posts?per_page=3'
-  //     ]
-
-  //     //pass that array of urls into promise.all (should return 1 object of data I need)
-  //     let requests = urls.map((url) => fetch(url));
-
-  //     Promise.all(requests)
-  //       .then(responses => console.log(responses))
-  // }
-
-  useEffect(() => {
-    const PROJECTS_URL = 'https://wordpress-dot-jacwynn-site-263723.appspot.com/wp-json/wp/v2/project'
-    fetch(PROJECTS_URL)
-      .then(response => response.json())
-      .then(response => {
-        setProject(response)
-      })
-  }, []);
-
-  const { headerMenu } = props;
+  const { headerMenu, data } = props;
+  const projects = data[0];
 
   return (
     <Layout>
@@ -49,6 +25,19 @@ const Index = props => {
   )
 }
 
+Index.getInitialProps = async () => {
+
+  const urls = [
+    fetch('https://wordpress-dot-jacwynn-site-263723.appspot.com/wp-json/wp/v2/project').then((response) => response.json()),
+    fetch('https://wordpress-dot-jacwynn-site-263723.appspot.com/wp-json/wp/v2/posts?per_page=3').then((response) => response.json())
+  ]
+
+  const data = await Promise.all(urls);
+
+  return {
+    data
+  };
+};
   
 
 export default PageWrapper(Index);

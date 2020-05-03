7a819dbfcd6c9ef077986bf1ce92af0de30af19b
jacwynn <jac@wynndigitalweb.com>
2020-05-03 14:30:06 -0400

    minor content update on background; fix images on mobile in detailed post

--- frontend/components/Services.js
+++ frontend/components/Services.js
@@ -2,7 +2,7 @@
 const Services = () => (
     <section className="services-section" id="services">
         <h2>Background</h2>
-        <p>Started learning how to code in summer of 2014. A year later I began to freelance and built custom WordPress sites for local businesses in the Pittsburgh area. In 2016 I got a chance to work in a fast paced creative agency and I was able to pick up a lot of knowledge about the whole development process. As of now, I work full-time at DEG Digital as a SFCC Front End Developer.</p>
+        <p>I started learning how to code in summer of 2014. A year later I began to freelance and built custom WordPress sites for local businesses in the Pittsburgh area. In 2016 I got a chance to work in a fast paced creative agency and I was able to pick up a lot of knowledge about the whole development process. As of now, I work full-time at DEG Digital as a SFCC Front End Developer.</p>
         <p>When it comes to a favorite language/technology, I'd have to say I enjoy using React. If I were to start a project today, I would use the MERN stack. I also enjoy finding ways to deploy and host websites more efficently.</p>
         <p>Whenever I'm not in front of a computer, you can find me on the basketball court or playing chess on chess.com.</p>
         {/* <div className="services wrap">
